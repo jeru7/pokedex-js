b275666f22df64c355d59d4d07dae1cf0811ad2d
jeru7 <john.ungab7@gmail.com>
2023-12-08 11:01:41 +0800

    poke stats function added

--- js/app.js
+++ js/app.js
@@ -12,6 +12,9 @@ class Pokedex {
     this.pokeName = document.querySelector("#pokeName");
     this.pokeGif = document.querySelector("#pokeGif");
     this.pokeId = document.querySelector("#pokeId");
+    this.pokeStatsConstainer = document.querySelector(
+      ".main__container__displayStats__pokeStats__stats"
+    );
   }
 
   runPokedex() {
@@ -60,17 +63,57 @@ class Pokedex {
     this.pokeName.innerText = pokeName;
     this.pokeGif.src = `./sprites/sprites/pokemon/other/showdown/${pokeId}.gif`;
 
-    if (pokeId < 10) {
-      this.pokeId.innerText = `No. 00${pokeId}`;
-    } else if (pokeId < 100) {
-      this.pokeId.innerText = `No. 0${pokeId}`;
-    } else {
-      this.pokeId.innerText = `No. ${pokeId}`;
-    }
+    this.displayPokemonID(pokeId);
+    this.displayPokemonSTATS(data);
 
     console.log(data);
 
     // pokemon stats
+  }
+
+  displayPokemonSTATS(data) {
+    this.pokeStatsConstainer.innerHTML = "";
+
+    data.stats.forEach((stat) => {
+      const statInfo = stat.stat;
+      const statName = statInfo.name.toUpperCase();
+      const statValue = stat.base_stat;
+
+      const pokeStatsInfo = document.createElement("p");
+      pokeStatsInfo.classList.add("pokeStatsInfo");
+
+      const pokeStatsName = document.createElement("span");
+      pokeStatsName.classList.add("pokeStatsName");
+
+      const pokeStatsValue = document.createElement("span");
+      pokeStatsValue.classList.add("pokeStats");
+
+      if (statName.includes("SPECIAL")) {
+        pokeStatsName.innerText = `${statName.replace(
+          /SPECIAL[-\s]*/g,
+          "SPECIAL "
+        )}: `;
+      } else {
+        pokeStatsName.innerText = `${statName}: `;
+      }
+
+      pokeStatsValue.innerText = `${statValue}`;
+
+      pokeStatsInfo.appendChild(pokeStatsName);
+      pokeStatsInfo.appendChild(pokeStatsValue);
+
+      this.pokeStatsConstainer.appendChild(pokeStatsInfo);
+    });
+  }
+
+  displayPokemonID(id) {
+    if (id < 10) {
+      this.pokeId.innerText = `No. 00${id}`;
+    } else if (id < 100) {
+      this.pokeId.innerText = `No. 0${id}`;
+    } else {
+      this.pokeId.innerText = `No. ${id}`;
+    }
   }
 }
 
