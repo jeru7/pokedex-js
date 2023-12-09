7c2355a73437ed77b5c821a94a35ea6befeb53c0
jeru7 <john.ungab7@gmail.com>
2023-12-09 19:21:20 +0800

    display poketypes funtion done

--- js/app.js
+++ js/app.js
@@ -15,6 +15,9 @@ class Pokedex {
     this.pokeStatsConstainer = document.querySelector(
       ".main__container__displayStats__pokeStats__stats"
     );
+    this.pokeTypesContainer = document.querySelector(
+      ".main__container__displayStats__pokeStats__type"
+    );
   }
 
   runPokedex() {
@@ -61,14 +64,93 @@ class Pokedex {
     const pokeName = data.name.charAt(0).toUpperCase() + data.name.slice(1);
     this.pokeImage.src = `./sprites/sprites/pokemon/other/official-artwork/${pokeId}.png`;
     this.pokeName.innerText = pokeName;
+
+    if (pokeName === "Kyogre") {
+      this.pokeGif.style.width = "250px";
+    } else {
+      this.pokeGif.style.width = "200px";
+    }
+
     this.pokeGif.src = `./sprites/sprites/pokemon/other/showdown/${pokeId}.gif`;
 
     this.displayPokemonID(pokeId);
     this.displayPokemonSTATS(data);
+    this.displayPokemonTYPES(data);
 
     console.log(data);
 
     // pokemon stats
+  }
+
+  displayPokemonTYPES(data) {
+    this.pokeTypesContainer.innerHTML = "";
+
+    data.types.forEach((dataType) => {
+      const typeName = dataType.type.name;
+
+      const typeElement = document.createElement("p");
+      typeElement.classList.add("pokeTypes");
+
+      typeElement.innerText = `${typeName.toUpperCase()}`;
+      switch (typeName) {
+        case "normal":
+          typeElement.style.backgroundColor = "#a8a878";
+          break;
+        case "fire":
+          typeElement.style.backgroundColor = "#f08030";
+          break;
+        case "water":
+          typeElement.style.backgroundColor = "#6890f0";
+          break;
+        case "electric":
+          typeElement.style.backgroundColor = "#f8d030";
+          break;
+        case "grass":
+          typeElement.style.backgroundColor = "#78c850";
+          break;
+        case "ice":
+          typeElement.style.backgroundColor = "#98d8d8";
+          break;
+        case "fighting":
+          typeElement.style.backgroundColor = "#c03028";
+          break;
+        case "poison":
+          typeElement.style.backgroundColor = "#a040a0";
+          break;
+        case "ground":
+          typeElement.style.backgroundColor = "#e0c068";
+          break;
+        case "flying":
+          typeElement.style.backgroundColor = "#a890f0";
+          break;
+        case "psychic":
+          typeElement.style.backgroundColor = "#f85888";
+          break;
+        case "bug":
+          typeElement.style.backgroundColor = "#a8b820";
+          break;
+        case "rock":
+          typeElement.style.backgroundColor = "#b8a038";
+          break;
+        case "ghost":
+          typeElement.style.backgroundColor = "#705898";
+          break;
+        case "dragon":
+          typeElement.style.backgroundColor = "#7038f8";
+          break;
+        case "dark":
+          typeElement.style.backgroundColor = "#705848";
+          break;
+        case "steel":
+          typeElement.style.backgroundColor = "#b8b8d0";
+          break;
+        case "fairy":
+          typeElement.style.backgroundColor = "#ee99ac";
+          break;
+      }
+
+      this.pokeTypesContainer.appendChild(typeElement);
+    });
   }
 
   displayPokemonSTATS(data) {
